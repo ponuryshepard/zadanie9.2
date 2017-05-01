fea103e3d6b800e9f4db9db9e507476f177103a9
patryk <perseusz91@gmail.com>
2017-05-01 11:31:18 +0200

    zadanie9.2

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+
+var arr = allNames.concat(femaleNames,maleNames);
+var newName = 'Marian'
+
+if ( allNames.indexOf(newName) === -1 ) {
+    var newName = allNames.push('Marian');
+} 
